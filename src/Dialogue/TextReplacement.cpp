270c58bce05ef64ad715d88bed3bc229b421e437
Scrabx3 <kris.vlntn@gmail.com>
2024-12-05 14:47:13 +0100

    make type non-optional

--- src/Dialogue/TextReplacement.cpp
+++ src/Dialogue/TextReplacement.cpp
@@ -4,12 +4,14 @@
 
 namespace DDR
 {
-  TextReplacement::TextReplacement(const YAML::Node& a_node) :
-    _script(a_node["script"].as<std::string>()),
-    _speakerId(Util::FormFromString(a_node["speaker"].as<std::string>(""))),
-    _targetId(Util::FormFromString(a_node["target"].as<std::string>(""))),
-    _type(magic_enum::enum_cast<ReplacemenType>(a_node["type"].as<int>(0)).value_or(ReplacemenType::Any))
-  {
+	TextReplacement::TextReplacement(const YAML::Node& a_node) :
+		_script(a_node["script"].as<std::string>()),
+		_speakerId(Util::FormFromString(a_node["speaker"].as<std::string>(""))),
+		_targetId(Util::FormFromString(a_node["target"].as<std::string>(""))),
+		_type(magic_enum::enum_cast<ReplacemenType>(a_node["type"].as<int>()).or_else([]() -> std::optional<ReplacemenType> { 
+      throw std::runtime_error("Failed to load replacement type");
+    }).value())
+	{
     if (_script.empty()) {
       throw std::runtime_error("Failed to load script");
     }
